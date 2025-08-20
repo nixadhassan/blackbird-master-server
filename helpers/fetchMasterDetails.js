d1fbe04d2198c1dae62df6244204eaf4831e0c11
million-art <millionmulugeta09@gmail.com>
2025-08-20 21:48:11 +0300

    Revert "Removed logs"
    This reverts commit a65f22a0516ff6dba8bef9085135a04d8fc1d833.

--- helpers/fetchMasterDetails.js
+++ helpers/fetchMasterDetails.js
@@ -60,6 +60,7 @@ const fetchMasterDetails = async () => {
     }
   }
 
+  console.log(global.masterDetails)
 
   // Prevent multiple intervals (when re-invoked)
   if (global.masterDetailsInterval) {
